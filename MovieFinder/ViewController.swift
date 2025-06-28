641cc7182f6d4cd4d832a7bce8e35ec32ef8b21f
Larissa Kaweski <larissa.kaweski@inter.co>
2025-06-28 13:34:48 -0300

    Initial Commit

---
+++ MovieFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieFinder
+//
+//  Created by Larissa Kaweski Siqueira on 28/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
